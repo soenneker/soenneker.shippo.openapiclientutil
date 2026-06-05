64fc25d48ca9d3a68e778ec65190ec07115440aa
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:45 -0500

    xml commenting

--- src/Soenneker.Shippo.OpenApiClientUtil/Abstract/IShippoOpenApiClientUtil.cs
+++ src/Soenneker.Shippo.OpenApiClientUtil/Abstract/IShippoOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Shippo.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IShippoOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<ShippoOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
